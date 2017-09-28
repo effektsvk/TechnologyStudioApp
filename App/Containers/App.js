38f9b347058839e080e8a91a6a0b3d3560d5e441
Erik Slovák <erik.slovak7@gmail.com>
2017-09-28 17:28:45 +0200

    redux-saga test

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { AsyncStorage } from 'react-native';
+import { compose, createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import ReduxThunk from 'redux-thunk';
 import reducers from '../Redux';
 import Router from '../Navigation/Router';
+import { helloSaga } from '../Sagas/sagas';
 import '../Config/ReactotronConfig';
 
 class App extends Component {
 
 	render() {
-		const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+		const sagaMiddleware = createSagaMiddleware()
+		const store = createStore(
+			reducers,
+			{},
+			compose(
+				applyMiddleware(ReduxThunk, sagaMiddleware), autoRehydrate()
+			)
+		);
+
+		sagaMiddleware.run(helloSaga);
+		persistStore(store, {storage: AsyncStorage});
 
 		return(
 			<Provider store={store}>
 				<Router />
 			</Provider>
-		)
+		);
 	}
 }
 
